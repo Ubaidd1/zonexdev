855cd6fb3c351350c6954d940d7eacbd03bcd16c
obaidullah72 <zonexdev773@gmail.com>
2025-01-30 09:18:01 +0500

    i

--- Mainsite/js/vendor/appvalidation.js
+++
@@ -1,5 +0,0 @@
-﻿function ValidateEmail(email) {
-    // Validate email format
-    var expr = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
-    return expr.test(email);
-};
